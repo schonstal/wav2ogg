093eace8b5c78ff571fd198ba3d126c6d2891768
Josh Schonstal <josh@schonstal.org>
2017-08-26 14:56:20 -0700

    🐛 Require node 8

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ let filenames = [];
 let quality = 10;
 
 program
-  .version('0.1.0')
+  .version('0.1.1')
   .arguments('[files...]')
   .usage('<file ...>')
   .option('-f, --force', 'overwrite output files')
